18e1f55c71f689889e13577cd7b45056d6bc220f
KoSangWon <jackoss@naver.com>
2020-03-03 06:25:58 +0900

    make ControlTodo

--- components/Todo.js
+++ components/Todo.js
@@ -1,20 +1,47 @@
 import React, {Component} from "react";
-import { View, Text, StyleSheet, Dimensions, Platform } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Platform } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { TextInput } from "react-native-gesture-handler";
+import { TextInput, TouchableOpacity, ScrollView } from "react-native-gesture-handler";
+import ControlTodo from "./ControlTodo"
 
 const windowWidth = Dimensions.get('window').width;
 
 export default class Todo extends Component {
+    state = {
+        newToDo: ""
+    }
     render() {
+        const {newToDo} = this.state;
         return (
             <View style={styles.container}>
                 <View style={styles.card}>
-                    <TextInput style={styles.input} placeholder={"메모를 적어보세요"} />
+                    <View style={styles.column}>
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder={"메모를 적어보세요"} 
+                            value={newToDo} 
+                            onChangeText={this._controlNewToDo} 
+                            placeholderTextColor={"#999"}
+                            returnKeyType={"done"}
+                            autoCorrect={false}
+                        />
+                        <TouchableOpacity style={styles.addButton}>
+                            <Text style={styles.addText}>추가</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <ScrollView>
+                        <ControlTodo/>
+                    </ScrollView>
                 </View>
             </View>
         );
     }
+
+    _controlNewToDo = text => {
+        this.setState({
+            newToDo: text
+        });
+    };
 }
 
 const styles = StyleSheet.create({
@@ -29,21 +56,27 @@ const styles = StyleSheet.create({
         backgroundColor: '#fffff9',
         flex: 1,
         width: windowWidth - 50,
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
-        ...Platform.select({
-            ios: {
-                shadowColor: "rgb(30, 30, 30)",
-                shadowOpacity: 0.6,
-                shadowRadius: 6,
-                shadowOffset: {
-                    height: -1,
-                    width: 0
-                }
-            },
-            android: {
-                elevation: 3
-            }
-        })
+    }, 
+    input: {
+        flex:1,
+        padding: 20,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: 1,
+        fontSize: 18
+    },
+    column: {
+        flexDirection: 'row'
+    },
+    addButton: {
+        flex: 1,
+        backgroundColor: '#ff005d',
+        alignContent: 'center',
+        justifyContent: 'center',
+        width: 80
+    },
+    addText: {
+        fontSize: 18,
+        alignSelf: 'center',
+        color: 'white'
     }
 });
