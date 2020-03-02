a505d4d8cf9c61fbac3520949e06f337a1317e40
KoSangWon <jackoss@naver.com>
2020-03-03 02:37:43 +0900

    Home Screen Style

--- App.js
+++ App.js
@@ -3,20 +3,19 @@ import 'react-native-gesture-handler';
 import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Stack = createStackNavigator()
 
 const HomeScreen = ({navigation}) => (
   <View style={styles.home}>
     <StatusBar barStyle={"dark-content"}/>
-    <Button 
-      title="To Do List"
-      onPress={() => navigation.navigate('ToDoList')}
-    />
-    <Button 
-      title="Movie"
-      onPress={() => navigation.navigate('Movie')}
-    />
+    <TouchableOpacity style={styles.todoButton} onPress={() => navigation.navigate('ToDoList')}>
+      <Text style={styles.todoText}>To Do List</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.movieButton} onPress={() => navigation.navigate('Movie')}>
+      <Text style={styles.movieText}>Movie</Text>
+    </TouchableOpacity>
   </View>
 )
 
@@ -46,6 +45,41 @@ export default function App() {
 
 const styles = StyleSheet.create({
   home: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  todoButton:{
+    width: 280,
+    height: 60,
+    backgroundColor: "#ff005d",
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30
+  },
+  todoText:{
+    color: "#ffffff",
+    fontWeight: "700",
+    fontSize: 15
+  },
+  movieButton:{
+    width: 280,
+    height: 60,
+    backgroundColor: "#ff005d",
+    marginTop: 15,
+    marginBottom: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30
+
+  },
+  movieText:{
+    color: "#ffffff",
+    fontWeight: "700",
+    fontSize: 15
+  },
+  todo:{
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
